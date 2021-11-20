89f1347b88483ca3961043d487b08b1ce140044a
Stephen Bird <sebirdman@gmail.com>
2021-11-20 20:34:37 +0000

    Add shairport-sync, webgui and full config file support

--- run.sh
+++ run.sh
@@ -6,6 +6,4 @@ if [[ -n "${USERNAME:-}" ]] && [[ -n "${PASSWORD:-}" ]]; then
   credentials="\&username=$USERNAME\&password=$PASSWORD"
 fi
 
-sed -i "s,^source = .*,source = librespot:///librespot?name=Spotify\&devicename=$DEVICE_NAME\&bitrate=320\&volume=100$credentials," /etc/snapserver.conf
-
-exec snapserver
+exec snapserver --config /root/snapserver.conf
